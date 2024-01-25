c3bc5402bb3752351f7066d5fec8db7c92c37479
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2024-01-25 13:06:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
+using Syncfusion4Support;
+using Syncfusion4Support.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<SfDialogService>();
+
+var app = builder.Build();
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
